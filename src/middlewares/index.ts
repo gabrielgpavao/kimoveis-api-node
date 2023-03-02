7abeef0aaec7f50329295e014b64f688e44c88f1
Gabriel <gabpavao@outlook.com>
2023-03-02 16:11:10 -0300

    feat: verify address duplicity middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,11 +2,12 @@ import { NextFunction } from 'connect';
 import { Request, Response } from 'express';
 import { ZodTypeAny } from 'zod';
 import { AppDataSource } from '../data-source';
-import { User } from '../entities';
+import { Address, User } from '../entities';
 import { AppError } from '../errors';
 import { tUserRepo } from '../interfaces/users/users.interfaces';
 import jwt from 'jsonwebtoken'
 import 'dotenv/config'
+import { tAddressRepo } from '../interfaces/realEstates/realEstates.interfaces';
 
 const validateInputDataMiddleware = (schema: ZodTypeAny) => (request: Request, response: Response, next: NextFunction): void => {
 	request.body = schema.parse(request.body)
@@ -86,10 +87,38 @@ async function validateUserIdMiddleware (request: Request, response: Response, n
 	next()
 }
 
+async function verifyDuplicityAddressMiddleware (request: Request, response: Response, next: NextFunction): Promise<void> {
+	const addressRepository: tAddressRepo = AppDataSource.getRepository(Address)
+	
+	const { street, number, zipCode, city, state } = request.body.address
+	
+	const findAddress = number ? await addressRepository.findOne({ where: {
+		street: street,
+		zipCode: zipCode,
+		state: state,
+		city: city,
+		number: number
+	}}) : await addressRepository.findOne({ where: {
+		street: street,
+		zipCode: zipCode,
+		state: state,
+		city: city
+	}})
+	
+	console.log(request.body)
+	
+	if (findAddress) {
+		throw new AppError('Address already exists', 409)
+	}
+
+	next()
+}
+
 export {
 	validateInputDataMiddleware,
 	verifyEmailDuplicityMiddleware,
 	validateTokenMiddleware,
 	validateAdminPermissionMiddleware,
-	validateUserIdMiddleware
+	validateUserIdMiddleware,
+	verifyDuplicityAddressMiddleware
 }
